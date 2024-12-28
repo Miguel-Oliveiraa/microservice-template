c58e61b99f2aa5d605ad379532fe2cb861d29a05
Miguel-Oliveiraa <miguellgomes64@gmail.com>
2024-12-28 02:16:34 -0300

    feat: testing pipeline

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,6 +3,9 @@ import userController from './user/user.controller';
 
 const router = Router();
 
+router.get('/', (req, res) => {
+  res.send('Welcome to the API');
+});
 router.use('/users', userController);
 
 export default router;
